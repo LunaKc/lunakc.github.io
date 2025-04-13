abdf7f6dde3cb2085061c7c89182da079f7c1df7
Kc-Luna <luna.110@osu.edu>
2025-04-13 16:05:38 -0400

    upgrades people upgrades

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: Add a click event to all project links
-    document.querySelectorAll('.project a').forEach(item => {
-        item.addEventListener('click', function(e) {
-            alert('Prepare to be amazed by this project!');
-        });
-    });
-});
